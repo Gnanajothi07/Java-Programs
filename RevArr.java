2d512be3652a3655c8ca8eec3fac8b6a8fd2c623
Gnanajothi07 <120785724+Gnanajothi07@users.noreply.github.com>
2023-12-22 12:16:14 +0530

    Create RevArr.java

---
+++ RevArr.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class reverseArr {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+			
+		}
+		
+		reverse(arr);
+		System.out.print(Arrays.toString(arr));
+			
+	}
+	static void reverse(int[] arr) {
+		int start = 0;
+		int end = arr.length-1;
+		while(start<end) {
+			swap(arr,start,end);
+			start++;
+			end--;
+		}
+		
+	}
+	static void swap(int[] arr,int start,int end) {
+		int temp = arr[start];
+		arr[start] = arr[end];
+		arr[end] = temp;
+	}
+}
